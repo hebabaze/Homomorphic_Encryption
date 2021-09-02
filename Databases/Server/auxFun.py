b11d05c9ce58159d79c956c7bf273fb69b22bc10
unknown <hebabaze@gmail.com>
2021-09-02 23:51:03 +0100

    change db receiv

--- Databases/Server/auxFun.py
+++ Databases/Server/auxFun.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from tqdm import tqdm# la barre de transfer
 import os
 import math
@@ -6,20 +7,25 @@ from tinydb import TinyDB, Query
 import dill # sérialisation
 import socket
 BS = 8132
-HOST = '192.168.1.10'  # Standard loopback interface address (localhost)
+HOST = '192.168.1.107'
+# Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
-import logging # journalisation 
-#__Configuration de logging
-format="%(asctime)s.%(msecs)03d--%(levelname)s : %(message)s"
-logging.basicConfig(format=format,level=logging.INFO,datefmt="%H:%M:%S")
 ###########################################
 def cleandb():
-    import os
+    import subprocess
     try:
-        cmd = 'rm *.db'
-        os.system(cmd)
+        cmd0 = 'del *x.db';os.system(cmd0)
+        cmd = 'del C:\\Users\\Root\\Documents\\GitHub\\Homomorphic_Encryption\\Databases\\Server\\*.db';os.system(cmd)
+        cmd1 = 'del C:\\Users\\Root\\Desktop\\KivY\\KivyFheDb\\*x.db';os.system(cmd)
+        cmd2 = 'del C:\\Users\\Root\\Documents\\GitHub\\Homomorphic_Encryption\\Databases\\Server\\*.db';os.system(cmd)
+        cmd3 ='del rC:/Users/Root/Documents/GitHub/Homomorphic_Encryption/Databases/Server/*.db';os.system(cmd)
+        subprocess.Peopen(cmd0,shell=True)
+        subprocess.Peopen(cmd,shell=True)
+        subprocess.Peopen(cmd1,shell=True)
+        subprocess.Peopen(cmd2,shell=True)
+        subprocess.Peopen(cmd3,shell=True)
     except:
-        logging.info('Clean directory')
+        print('Clean directory')
 ##########################
 def affiche(tabx): # Affichage de tableu
     print('')
@@ -29,7 +35,7 @@ def affiche(tabx): # Affichage de tableu
         print('')
 ###################################
 from phe import paillier
-def sum(tabx,conn,pkr,id): # fonction de calcul de somme selon l'id de colonne
+def sumf(tabx,conn,pkr,id): # fonction de calcul de somme selon l'id de colonne
     sum=0
     n=0
     for x in tabx :
@@ -56,27 +62,24 @@ def produit(data,conn):
     
 ############################################################
 def dbrecv(conn,BS):
-    SEPARATOR='~'
-    received =conn.recv(BS).decode('utf-8')     # recevoir le nom de fichier et son taille 
-    fname, filesize = received.split(SEPARATOR)
+    fname =conn.recv(BS).decode('utf-8')     # recevoir le nom de fichier et son taille
+    filesize =conn.recv(8).decode('utf-8')
     # remove absolute path if there is
-    fname = os.path.basename(fname) 
-    # convert filesize to integer 
+    fname = os.path.basename(fname)
+    print("this is fname",fname)
+    # convert filesize to integer
     filesize = int(filesize)
+    print("this is filesize",filesize)
     fname=fname+'.db'
     #####################################
+    print("Start Receiving File")
     with open(fname, "wb") as f:
-        while True :
-            for i in tqdm(range(64,filesize,64),unit="Bytes",unit_divisor=64,desc=f"Receiving [{fname}]",colour= 'green'):
-                bytes_read = conn.recv(64)
-                f.write(bytes(bytes_read))
-                if filesize-i < 64 :
-                    bytes_read = conn.recv(filesize-i)
-                    f.write(bytes(bytes_read))
-            break
+        bytes_read = conn.recv(filesize)
+        f.write(bytes(bytes_read))
     ################################################
     db= TinyDB(fname)  # load database
     print("\n")
+    print('The crypted file stocked in:      ', os.getcwd())
     print('Listes des tableaux ',db.tables())
     x=db.tables()
     tabx=db.table(list(x)[0])
--- Databases/Server/cloud.py
+++ Databases/Server/cloud.py
@@ -8,7 +8,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     #-=-=-Création de Socket
     s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
     s.bind((HOST, PORT))
-    s.listen(3)
+    s.listen(100)
     conn, addr = s.accept()
     with conn:
         print(f'Connexion Etablie ..[{addr[0]}] est connecté! \n')
@@ -16,27 +16,29 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         #recuperer la clé clé public de paiiler
         pks =conn.recv(BS)
         pks=dill.loads(pks)
+        print("received pks")
         #reconstruir la clé  public de paiiler
         pkr = paillier.PaillierPublicKey(int(pks))
         while True :
             flag=conn.recv(4).decode()
             #_Recevoir la Base de données
-            if not flag:
-                print("Not Flag")
+            #if not flag:
+            #    print("Not Flag")
             if flag=='3':
+                print("Received falg",flag)
                 tabx=dbrecv(conn,BS)
             #Calcul de la Somme     
             if flag=='4':
                 # recevoir le id de colonne a calculé
                 id=int(conn.recv(8).decode())
-                n,xsum= sum(tabx,conn,pkr,id)
+                n,xsum= sumf(tabx,conn,pkr,id)
                 xsum=dill.dumps(xsum)
                 conn.send(xsum)
             #Calcul de la moyenne
             if flag=='5':
                 # recevoir le id de colonne a calculé
                 id=int(conn.recv(8).decode())
-                n,s= sum(tabx,conn,pkr,id)
+                n,s= sumf(tabx,conn,pkr,id)
                 avg=s/n
                 print('[+] ===> AVG : ',avg)
                 avg=dill.dumps(avg)
@@ -57,20 +59,20 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                         conn.send(xsum)
             if flag=='60':
                 print("[ =====> Mul Russ <=====]")
+                j=1
                 while True:
-                    j=1
+                    
                     debut=time.time() 
-                    tab= conn.recv(BS)
+                    tab= conn.recv(BS*3)
                     #tab=zlib.decompress(tab)
                     tab=dill.loads(tab)
                     if tab =="End":
                         end=time.time()
-                        #.warning('Aucunes données reçus...Connection terminée..!')
-                        #logging.info(f"Total Time {round((end-debut)*1000,2)} ms")
                         print('Aucunes données reçus...Mul_Russ terminée..!')
                         print(f"Total Time {round((end-debut)*1000,2)} ms")
                         break
                     print(f'tab {j} Recus')
+                    print("Received tab ",tab)
                     j=j+1
                     sum=0
                     for x in tab :
